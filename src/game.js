4eec9f1f55054f95bd9ff476b59fc5f4511df691
AAD2000 <aadanilov00@outlook.com>
2020-05-10 17:58:05 +0300

    fixed for safari

--- src/game.js
+++ src/game.js
@@ -1,12 +1,14 @@
 
 export default class Game {
-    width = 50;
-    height = 50;
+    constructor(){
+        this.width = 50;
+        this.height = 50;
 
-    player = {
-        x: 0,
-        y: 0
-    };
+        this.player = {
+            x: 0,
+            y: 0
+        };
+    }
 
     moveUp() {
         if (this.player.y > 0) {
